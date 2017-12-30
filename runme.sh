acee6398e6d34d246e554a0d3ecba2c97f2cd863
parsiya <parsiya@gmail.com>
2017-12-30 01:48:51 -0500

    Upload site.css as text/css

--- runme.sh
+++ runme.sh
@@ -2,4 +2,7 @@ echo "Starting build"
 eval '.\/tools\/build'
 echo "Starting upload"
 eval 'python s3cmd sync --acl-public --delete-removed -MP --no-preserve --rr public\/ s3:\/\/securitygobyexample.com'
+eval 'python s3cmd --acl-public --no-preserve --cf-invalidate --add-header="Expires: Sat, 20 Nov 2286 19:00:00 GMT" --mime-type="text/css" put public\/site.css s3:\/\/securitygobyexample.com\/site.css'
+
+
 
